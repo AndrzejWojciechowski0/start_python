bd1acf429feabf66cc22023fb5431a0e1677b6ba
AndrzejWojciechowski0 <wojciechowski969@gmail.com>
2019-03-14 19:45:18 +0100

    safadffdsasfsfafsfsafsfafs

--- my_cli_calc.py
+++ my_cli_calc.py
@@ -1,8 +1,8 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='My calculator cli')
-parser.add_argument('--add', type=int, nargs=2)
+parser = argparse.ArgumentParser(description="My calculator cli")
+parser.add_argument("--add", type=int, nargs=2)
 
 args = parser.parse_args()
 
-print(sum(args.add))
+print(args.add)
--- reset_content.py
+++ reset_content.py
@@ -1,7 +1,8 @@
 from os import path
 
-print(path.exists('.\my_name.txt'))
+print(path.exists("\my_name.txt"))
 
 
-my_name = open("my_name.txt", "w"):
+
+my_name = open("my_name.txt", "w")
 my_name.close()
